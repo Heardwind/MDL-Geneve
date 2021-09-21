bb3aa6faa0ba13f5ca249e100a18c23afa93a796
artemzhdanov <jubileewinsthere@gmail.com>
2021-09-21 10:24:35 +0300

    upd modal

--- js/common.js
+++ js/common.js
@@ -1,25 +1,53 @@
 
 
-let toggleNavButton = document.querySelector('.toggle_nav');
-let overlay = document.querySelector('.overlay');
-let moreButtons = document.querySelectorAll('.more');
+function init() {
 
-const showMoreHandler = function () {
-    const prevEl = this.previousElementSibling;
-    prevEl.style.webkitLineClamp = 13;
-    this.remove();
+    let toggleNavButton = document.querySelector('.toggle_nav');
+    let openModalButton = document.querySelector('.action_btn');
+    let closeModalButton = document.querySelector('.close_modal');
+    let overlay = document.querySelector('.overlay');
+    let moreButtons = document.querySelectorAll('.more');
+
+    const closeAll = function (e) {
+        e.preventDefault();
+        document.querySelector('body').classList.remove('nav_open');
+        document.querySelector('body').classList.remove('modal_open');
+    }
+
+    const showMoreHandler = function () {
+        const prevEl = this.previousElementSibling;
+        prevEl.style.webkitLineClamp = 13;
+        this.remove();
+    }
+
+    const toggleNavHandler = function (e) {
+        e.preventDefault();
+        document.querySelector('body').classList.toggle('nav_open');
+    }
+
+    const openModalHandler = function (e) {
+        e.preventDefault();
+        document.querySelector('body').classList.toggle('modal_open');
+    }
+
+    if(toggleNavButton) {
+        toggleNavButton.addEventListener('click', toggleNavHandler);
+    }
+    if(openModalButton) {
+        openModalButton.addEventListener('click', openModalHandler);
+    }
+    if(overlay) {
+        overlay.addEventListener('click', closeAll);
+    }
+    if(closeModalButton) {
+        closeModalButton.addEventListener('click', closeAll);
+    }
+    if(moreButtons.length) {
+        Array.from(moreButtons).map(elem => {
+            elem.addEventListener('click',showMoreHandler);
+        })
+    }
+
 }
 
-const toggleNavHandler = function (e) {
-    e.preventDefault();
-    document.querySelector('body').classList.toggle('nav_open');
-}
-
-
-toggleNavButton.addEventListener('click', toggleNavHandler);
-overlay.addEventListener('click', toggleNavHandler);
-if(moreButtons.length) {
-    Array.from(moreButtons).map(elem => {
-        elem.addEventListener('click',showMoreHandler);
-    })
-}
+document.addEventListener("DOMContentLoaded", init);
